ec3e94506de6084cbbd72a01949046a19e90d458
unknown <naresh949468@gmail.com>
2024-04-16 16:17:31 +0530

    hello world chnged

--- 2.variables.sh
+++ 2.variables.sh
@@ -1,2 +1,7 @@
-variables chnages
-sh is shell ki extension
+#!/bin/bash
+
+echo "ramesh : hello ramu how r u"
+echo "suesh : im fin and and u ramesh"
+echo "ramesh : wt dong ramesh"
+echo "suresh : notdong yo ramesh"
+
