00fd207fe6202319b5ba8e4e8678a4a15cd1b47c
unknown <naresh949468@gmail.com>
2024-04-16 16:30:26 +0530

    hello world chnged

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+person1=ramesh
+person2=suresh
+
+echo "$person1 : hello $person1 how r u"
+echo "$person2 : im fin and and u $person1"
+echo "$person1 : wt dong $person1"
+echo "$person2 : notdong yo $person1"
