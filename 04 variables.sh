a425e57aaae082a2b7d6bcd5fa915b36c4c19da9
unknown <naresh949468@gmail.com>
2024-04-16 16:44:07 +0530

    hello world chnged

---
+++ 04 variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+person1=$1
+person2=$2
+
+echo "$person1 : hello $person1 how r u"
+echo "$person2 : im fin and and u $person1"
+echo "$person1 : wt dong $person1"
+echo "$person2 : notdong yo $person1"
